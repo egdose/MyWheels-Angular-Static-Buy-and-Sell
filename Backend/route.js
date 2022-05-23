80c7bb8321d0eac9b78b8d9ed3dfba265606fab5
egdose <i180504@nu.edu.pk>
2022-05-23 13:41:09 +0500

    Backend done and tested
    Backend done and tested

--- Backend/route.js
+++ Backend/route.js
@@ -37,29 +37,84 @@ router.post('/add', (req, res, next) => {
 
 //Read all documents
 router.get('/getVehicles', (req, res, next) => {
-    Vehicle.find((err, vehicles) => {
+    var vehicles = Vehicle.find().select({ "_id": 0, "__v": 0 });
+
+    vehicles.exec(function (err, vehicle) {
         if (err) {
-            res.json(err);
-        }
-        else {
-            res.json(vehicles);
+            res.json({
+                msg: 'Failed to get vehicles'
+            });
+        } else {
+            res.json(vehicle);
         }
     });
 });
 
 //Read a document
-router.get('/getVehicle/:id', (req, res, next) => {
-    res.send('Get a vehicle');
+router.get('/getVehicle', (req, res, next) => {
+    var id = req.query.id;
+    var vehicle = Vehicle.find({ "id": id }).select({ "_id": 0, "__v": 0 });
+
+    vehicle.exec(function (err, vehicle) {
+        if (err) {
+            res.json({
+                msg: 'Failed to get vehicle'
+            });
+        } else {
+            res.json(vehicle);
+        }
+    });
 });
 
 //Update a document
 router.post('/update', (req, res, next) => {
-    res.send('Update a vehicle');
+    var id = req.body.id;
+    var vehicle = Vehicle.findOneAndUpdate({ "id": id }, {
+        $set: {
+            title: req.body.title,
+            image: req.body.image,
+            city: req.body.city,
+            price: req.body.price,
+            dateAdded: req.body.dateAdded,
+            rating: req.body.rating,
+            make: req.body.make,
+            year: req.body.year,
+            mileage: req.body.mileage,
+            color: req.body.color,
+            engine: req.body.engine,
+            type: req.body.type
+        }
+    }, { new: true });
+
+    vehicle.exec(function (err, vehicle) {
+        if (err) {
+            res.json({
+                msg: 'Failed to update vehicle'
+            });
+        } else {
+            res.json({
+                msg: 'Vehicle updated successfully'
+            });
+        }
+    });
 });
 
 //Delete a document
-router.post('/delete', (req, res, next) => {
-    res.send('Delete a vehicle');
+router.get('/delete', (req, res, next) => {
+    var id = req.query.id;
+    var vehicle = Vehicle.findOneAndDelete({ "id": id });
+
+    vehicle.exec(function (err, vehicle) {
+        if (err) {
+            res.json({
+                msg: 'Failed to delete vehicle'
+            });
+        } else {
+            res.json({
+                msg: 'Vehicle deleted successfully'
+            });
+        }
+    });
 });
 
 
